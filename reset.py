d4622d37afa5774f97f283aaa0ce114e349f4cf9
Baris Ceviz <me@barisceviz.com>
2018-09-20 15:26:43 +0300

    Added Reset script

---
+++ reset.py
@@ -0,0 +1,32 @@
+from src.db import SQLiteDatabase
+from termcolor import colored
+
+if __name__ == '__main__':
+  db = SQLiteDatabase()
+
+  db.query("DROP TABLE IF EXISTS songs;")
+  print colored('Dropped songs table',"red");
+
+  db.query("""
+    CREATE TABLE songs (
+      id  INTEGER PRIMARY KEY AUTOINCREMENT,
+      name  TEXT,
+      filehash  TEXT
+    );
+  """)
+  print colored('Created songs table',"green");
+
+  db.query("DROP TABLE IF EXISTS fingerprints;")
+  print colored('Removed fingerprints',"red");
+
+  db.query("""
+    CREATE TABLE `fingerprints` (
+      `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
+      `song_fk` INTEGER,
+      `hash`  TEXT,
+      `offset`  INTEGER
+    );
+  """)
+  print colored('Created Fingerprints',"green");
+
+  print colored('Done',"green");
